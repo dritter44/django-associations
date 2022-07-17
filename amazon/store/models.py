a63714c4f143049e2212ca681bf6dcbfd03d4444
dritter44 <dreither44@gmail.com>
2022-07-16 22:00:01 -0500

    amazon done

--- amazon/store/models.py
+++ amazon/store/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+
+class Shop(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shop')
+
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='products')
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviewed_products')
