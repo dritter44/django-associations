3642c7ec76d33e0ec623110db3cb5c75ce87226c
dritter44 <dreither44@gmail.com>
2022-07-16 20:26:32 -0500

    got medium working

--- medium/blog/models.py
+++ medium/blog/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete= models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
